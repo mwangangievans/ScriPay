f1082285a0f26f3005289b28cc9b34818325594a
mwangangi <muindemwangangi2@gmail.com>
2025-02-04 23:11:47 +0300

    register page

--- src/app/Shared/home/home.component.ts
+++ src/app/Shared/home/home.component.ts
@@ -4,7 +4,7 @@ import { HeaderComponent } from '../header/header.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent ],
+  imports: [HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './Shared/home/home.component';
+import { RegisterComponent } from './Shared/Auth/register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , HomeComponent],
+  imports: [RouterOutlet , HomeComponent ,RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
